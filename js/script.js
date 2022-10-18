253c2b6b8248dc580c253c10abf81cfcc2848b2f
Raylander <raylander.guimaraes@gmail.com>
2022-10-18 10:39:59 -0300

    adiciona descricoes servicos

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@ let servicos = document.getElementById("servicos");
 let contato = document.getElementById("contato");
 let navbarCollapse = document.getElementById("navbarNav");
 
+
 function hiddenMenu() {
     navbarCollapse.classList.remove("show")
 }
+
+
+
